0eab4375753441c6168bd674a0011a265640e5ad
giamaf <gianluca.maffucci@hotmail.com>
2024-02-08 11:20:16 +0100

    added store + api data

---
+++ src/assets/data/index.js
@@ -0,0 +1,8 @@
+const api = {
+    genEndpoint: 'https://api.themoviedb.org/3',
+    baseMovies: '/search/movie',
+    baseSeries: '/search/tv',
+    apiKey: '95c1c036557c10b399227d01c2792073'
+}
+
+export { api };
--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -2,5 +2,5 @@ import { reactive } from "vue";
 
 export const store = reactive({
     movies: [],
-    tvSeries: []
+    series: []
 });
