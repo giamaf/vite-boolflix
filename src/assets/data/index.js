4f6ddaae24e5de9c121ab250fa70c55e2e7c5aeb
giamaf <gianluca.maffucci@hotmail.com>
2024-02-08 15:07:10 +0100

    added api data

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,8 +1,10 @@
 const api = {
-    genEndpoint: 'https://api.themoviedb.org/3',
-    baseMovies: '/search/movie',
-    baseSeries: '/search/tv',
-    apiKey: '95c1c036557c10b399227d01c2792073'
+    baseUri: 'https://api.themoviedb.org/3',
+    moviesEndpoint: '/search/movie',
+    seriesEndpoint: '/search/tv',
+    apiKey: '95c1c036557c10b399227d01c2792073',
+    langIta: 'it-IT',
+    langEng: 'en-US',
 }
 
 export { api };
